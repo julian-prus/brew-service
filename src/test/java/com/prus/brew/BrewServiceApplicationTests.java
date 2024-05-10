eead044104ac93a7c1000baeb26ea381746836e0
Julian Prus <julian.m.prus@gmail.com>
2024-05-10 16:56:09 +0200

    add files

---
+++ src/test/java/com/prus/brew/BrewServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.prus.brew;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BrewServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
